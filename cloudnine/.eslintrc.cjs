64b2c07349bcba5e89176178c45381f1206017fa
imanwarsame <iwarsame38@gmail.com>
2023-04-10 19:33:05 +0100

    Connected to API successfully

---
+++ cloudnine/.eslintrc.cjs
@@ -0,0 +1,47 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'react/prop-types': 0,
+		'semi': [
+			'error',
+			'always'
+		],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+	}
+};
--- cloudnine/src/App.jsx
+++ cloudnine/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0)
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function formatDate(d) {
+	const day = days[d.getDay()];
+	const date = d.getDate();
+	const month = months[d.getMonth()];
+	const year = d.getFullYear();
+	return `${month} ${date}, ${year} | ${day}`;
 }
 
-export default App
+export default function App() {
+	const [lat, setLat] = useState();
+	const [long, setLong] = useState();
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			setLat(position.coords.latitude);
+			setLong(position.coords.longitude);
+		});
+	}, []);
+
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const connectionString = `${import.meta.env.VITE_WEATHER_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${import.meta.env.VITE_WEATHER_API_KEY}`;
+
+			await axios.get(connectionString)
+				.then(result => {
+					setData(result.data);
+					console.log(result.data);
+				});
+		};
+
+		if (lat && long) {
+			fetchData();
+		}
+
+	}, [lat,long]);
+
+	return (
+		<div>
+			<Typography variant='h4'>{formatDate(new Date())}</Typography>
+			{(typeof data.main !== 'undefined') ? (
+				<Box>
+					<Typography>{Math.round(data.main.temp)}°C</Typography>
+					<Typography>{data.name}</Typography>
+				</Box>
+			): (
+				<div></div>
+			)}
+		</div>
+	);
+}
+
+// function App() {
+// 	const [weather, setWeather] = useState(null);
+// 	useEffect(() => {
+// 		getLocalWeather().then(setWeather, console.error);
+// 	}, []);
+
+// 	return (
+// 		<div className="App">
+// 			<Typography>{formatDate(new Date())}</Typography>
+// 			{(typeof weather.main !== 'undefined') ? <Typography>{Math.round(weather.main.temp)}°C</Typography> : null}
+// 			{/* <Typography>{lat}</Typography>
+// 			<Typography>{long}</Typography> */}
+// 			{/* <Typography>Temperature: {data.main.temp}°C</Typography> */}
+// 		</div>
+// 	);
+// }
+
+// export default App;
