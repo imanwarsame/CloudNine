2ca9b6707a0a2bf43ea561df05d19877c21a70af
imanwarsame <iwarsame38@gmail.com>
2023-04-24 21:43:59 +0100

    Fixed toggle position and made design responsive

--- cloudnine/src/Components/ThemeToggle.jsx
+++ cloudnine/src/Components/ThemeToggle.jsx
@@ -9,14 +9,9 @@ export default function ThemeToggle() {
 	const mode = useSelector(state => state.colour_theme);
 	const dispatch = useDispatch();
 
-	const fabStyle = {
-		right: 20,
-		bottom: 20,
-		position: 'fixed'
-	};
-
 	return(
-		<IconButton disableRipple style={fabStyle} sx={{ '&:hover': { backgroundColor: 'transparent' } }} onClick={() => dispatch(toggleColourTheme())}>
+		<IconButton disableRipple sx={{ '&:hover': { backgroundColor: 'transparent' }, right: '1%',
+			bottom: '1%', position: 'fixed' }} onClick={() => dispatch(toggleColourTheme())}>
 			{mode === 'light' ? <MoonIcon/> : <SunIcon/>}
 		</IconButton>
 	);
--- cloudnine/src/Components/WeatherCard.jsx
+++ cloudnine/src/Components/WeatherCard.jsx
@@ -7,28 +7,38 @@ import WeatherIcon from './WeatherIcon';
 export default function WeatherCard({ data, city }) {
 	const dispatch = useDispatch();
 
+	function convertDate(dt) {
+		const h = dt.getUTCHours();
+		const m = dt.getUTCMinutes();
+
+		return ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) ;
+	}
+
 	const checkIfDay = () => {
 		let dayTime = false;
 
-		if (new Date().valueOf() / 1000 < data.sys.sunset) {
+		const localCurrent = new Date((new Date().valueOf()/1000 + data.timezone) * 1000).toISOString();
+		const localSunrise = new Date((data.sys.sunrise + data.timezone) * 1000).toISOString();
+		const localSunset = new Date((data.sys.sunset + data.timezone) * 1000).toISOString();
+
+		if (localCurrent < localSunset && localCurrent > localSunrise) {
 			dayTime = true;
 		} else {
 			dayTime = false;
 		}
 
-		console.log(city);
 		return dayTime;
 	};
 
 	return (
-		<Grid container spacing={2} sx={{ width: '75%' }}>
+		<Grid container spacing={2} sx={{ width: '75%', mb: 5 }}>
 			<Grid item xs={12}>
 				<Paper sx={{ p: 2 }}>
 					<Grid container>
-						<Grid item xs={6}>
+						<Grid item xs={12} md={6}>
 							<WeatherIcon weatherID={data.weather[0].id} dayTime={checkIfDay()}/>
 						</Grid>
-						<Grid item xs={6}>
+						<Grid item xs={12} md={6}>
 							<Typography>{Math.round(data.main.temp)}°C</Typography>
 							{(city === '') ? (<Typography>{data.name}</Typography>): (<Typography>{city}</Typography>)}
 							<Button variant='outlined' color='secondary' onClick={() => dispatch(setNotification('success', 'This worked', 4))}>Hello</Button>
@@ -36,23 +46,23 @@ export default function WeatherCard({ data, city }) {
 					</Grid>
 				</Paper>
 			</Grid>
-			<Grid item xs={3}>
+			<Grid item xs={12} md={3}>
 				<Paper sx={{ p: 2 }}>
-					<Typography>Sunrise: {new Date((data.sys.sunrise + data.timezone) * 1000).toLocaleTimeString(navigator.language, { hour: '2-digit', minute:'2-digit' })}</Typography>
-					<Typography>Sunset: {new Date((data.sys.sunset + data.timezone) * 1000).toLocaleTimeString(navigator.language, { hour: '2-digit', minute:'2-digit' })}</Typography>
+					<Typography>Sunrise: {convertDate(new Date((data.sys.sunrise + data.timezone) * 1000))}</Typography>
+					<Typography>Sunset: {convertDate(new Date((data.sys.sunset + data.timezone) * 1000))}</Typography>
 				</Paper>
 			</Grid>
-			<Grid item xs={3}>
+			<Grid item xs={12} md={3}>
 				<Paper sx={{ p: 2 }}>
 					<Typography>Description: {data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)}</Typography>
 				</Paper>
 			</Grid>
-			<Grid item xs={3}>
+			<Grid item xs={12} md={3}>
 				<Paper sx={{ p: 2 }}>
 					<Typography>Humidity: {data.main.humidity}%</Typography>
 				</Paper>
 			</Grid>
-			<Grid item xs={3}>
+			<Grid item xs={12} md={3}>
 				<Paper sx={{ p: 2 }}>
 					<Typography>Wind speed: {Math.round(data.wind.speed * 2.23694)}mph</Typography>
 					<Typography>Direction: {data.wind.deg}°</Typography>
